c03d6241e3386c1963b48f3f92914f3f6c069b79
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-03-14 18:28:33 -0500

    Initial Commit

---
+++ emojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojiDictionary
+//
+//  Created by Barbara Feinstein on 3/14/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
