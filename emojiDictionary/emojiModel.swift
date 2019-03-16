c419e7f98d35f8a7de74e0526b1cb31fec0ec4e2
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-03-16 09:11:16 -0500

    initial commit

---
+++ emojiDictionary/emojiModel.swift
@@ -0,0 +1,35 @@
+//
+//  emojiModel.swift
+//  emojiDictionary
+//
+//  Created by Barbara Feinstein on 3/14/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import Foundation
+
+class Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+   
+       
+    
+    
+    init(symbol: String, name: String, description: String, usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+        
+        
+        
+    }
+    
+    
+    
+ 
+    
+}
